7f23c2ef47f7d90fba313dc8478739f3c6961920
Leonardo <lgomes.leo@gmail.com>
2018-02-02 22:23:52 -0200

    added more options to command line parsing and started xpc

--- lights-controller/src/main.c
+++ lights-controller/src/main.c
@@ -44,16 +44,45 @@ ws2811_t strip = {
 
 uv_loop_t* loop;
 uv_udp_t qpc;
-
+uv_tcp_t xpc;
 
 uv_buf_t alloc_buffer(uv_handle_t *handle, size_t suggested_size) {
   return uv_buf_init((char*) malloc(suggested_size), suggested_size);
 }
 
+void on_xpc_connection(uv_stream_t* connection, int status) {
+	int r;
+	uv_stream_t client;
+
+	if (status < 0) {
+		fprintf(stderr, "Error receiving XPC connection (status: %d).\n",
+			status);
+		return;
+	}
+
+	
+}
+
+void xpc_init() {
+	struct sockaddr_in addr;
+	int r;
+
+	xpc = uv_tcp_init(loop, &xpc);
+	uv_ip4_addr("0.0.0.0", XPC_PORT, &addr);
+
+	uv_tcp_bind(&xpc, &addr, 0);
+
+	if ((r = uv_listen((uv_stream_t*)&xpc, 10, on_xpc_connection))) {
+		fprintf(stderr, "Error listening to XPC connections: %s.\n",
+			uv_strerror(r));
+		exit(EXIT_FAILURE);
+	}
+}
+
 void qpc_packet_parse(const uv_buf_t* buf) {
 	uint8_t* buffer;
 	uint8_t op, channel;
-	uint16_t length;
+	uint16_t length, start, end;
 
 	buffer = (uint8_t*)buf->base;
 	if (buffer == NULL)
@@ -63,15 +92,30 @@ void qpc_packet_parse(const uv_buf_t* buf) {
 	channel = buffer[1];
 
 	switch (op) {
-	case 0:
-		length = *((uint16_t*)&(buffer[2]));
-		memcpy(strip.channel[channel].leds, &(buffer[4]), MIN(buf->len - 4, length));
+	case 0: // buffer start set: [op: 0][short: length][uint8_t*: ...]
+		length = *((uint16_t*)&(buffer[2])); // get given length
+		length = MIN(length, buf->len - 4); // get copy data length
+		length = MIN(length, strip.channel[channel].count); // get real copy len
+
+		memcpy(strip.channel[channel].leds, &(buffer[4]), length);
 		ws2811_render(&strip);
 		break;
-	case 255:
+	case 1: // buffer splice: [op: 1][short: start][short: end][uint8_t*: data]
+		start = *((uint16_t*)&(buffer[2]));
+		end = *((uint16_t*)&(buffer[4]));
+		length = end - start; // given length
+		length = MIN(buf->len - 6, length); // limited to data length
+		length = MIN(length, strip.channel[channel].count - start);
+
+		if (length > 0)
+			memcpy(strip.channel[channel].leds, &(buffer[6]), length);
+
+		ws2811_render(&strip);
+	case 255: // vendor specific
 		break;
 	default:
 		fprintf("request for undefined op\n");
+		break;
 	}
 }
 
@@ -80,7 +124,7 @@ void on_qpc_read(uv_handle_t* handle, ssize_t nread, const uv_buf_t* buf,
 	uv_udp_t* req;
 
 	req = (uv_udp_t*)handle;
-	
+
 	if (addr == NULL)
 		return;
 
@@ -105,11 +149,27 @@ void qpc_init() {
 }
 
 void print_help(char* argv0) {
-	printf("help: %s [[-x <port>] [-q <port>] [-v] [-h]]\n"
-	       "\t-x <port>     -- set XPC port\n"
-	       "\t-q <port>     -- set QPC port\n"
-	       "\t-v            -- set verbose mode\n"
-	       "\t-h            -- display this help text\n", argv0);
+	printf("help: %s [options]\n"
+	       "\t--xpc,-x <port>         -- set XPC port (default 7890)\n"
+	       "\t--qpc,-q <port>         -- set QPC port (default 7891)\n"
+	       "\t--verbose, -v           -- set verbose mode (default off)\n"
+	       "\t--help, -h              -- display this help text\n"
+	       "\t--freq,-f <freq>        -- set controller frequency (default 1200000)\n"
+	       "\t--dma,-d <num>          -- set dma channel (default 10)\n"
+	       "\t--ledcount,-c <count>     -- set the number of leds (default 50)\n"
+	       "\t--gpio,-g <pin>         -- set the gpio pin to use (default 10 -- SPI)\n"
+	       "\t--strip-type,-t <mode>  -- set strip mode (default BGR)\n"
+	       "\t--invert,-i             -- will invert the output at gpio\n"
+	       "\t--brightness,-b <value> -- set the brightness (default 255)\n"
+	       "\nremarks:\n"
+	       "\t* <port> are valid tcp/udp port values\n"
+	       "\t* <freq> is in Hz, so 800kHz would be 800000\n"
+	       "\t* be careful with dma channels, 10 seems to work just fine.\n"
+	       "\t* changing <pin> changes the method used by the server:\n"
+	       "\t  which means different permissions will be needed by the server\n"
+	       "\t  depending on the interface used, so keep that in mind.\n"
+	       "\t* valid <mode> values are: 'rgb', 'rbg', 'grb', 'gbr', 'brg', 'bgr'\n"
+	       "\t  'rgbw' and 'grbw'.\n", argv0);
 }
 
 void print_usage(char* argv0) {
@@ -117,27 +177,83 @@ void print_usage(char* argv0) {
 }
 
 void parse_parameters(int argc, char** argv) {
-	int c;
+	int c, option_index;
+	static struct option long_options[] = {
+		{"xpc", required_argument, NULL, 'x'},
+		{"qpc", required_argument, NULL, 'q'},
+		{"verbose", no_argument, &VERBOSE, 1},
+		{"help", no_argument, NULL, 'h'},
+		{"freq", required_argument, NULL, 'f'},
+		{"dma", required_argument, NULL, 'd'},
+		{"ledcount", required_argument, NULL, 'c'},
+		{"gpio", required_argument, NULL, 'g'},
+		{"strip-type", required_argument, NULL, 't'},
+		{"invert", no_argument, &(strip.channel[0].invert), 1},
+		{"brightness", required_argument, NULL, 'b'},
+		{0,0,0,0}
+	};
 
-	while ((c = getopt(argc, argv, "q:x:vh")) != -1) {
-		switch (c) {
-		case 'q':
-			QPC_PORT = atoi(optarg);
+	while (1) {
+		option_index = 0;
+		c = getopt_long(argc, argv, "x:q:vhf:d:l:g:m:ib:", long_options,
+			&option_index);
+
+		if (c == -1)
 			break;
+
+		switch (c) {
 		case 'x':
 			XPC_PORT = atoi(optarg);
+			break;
+		case 'q':
+			QPC_PORT = atoi(optarg);
 			break;
 		case 'v':
 			VERBOSE = 1;
 			break;
 		case 'h':
 			print_help(argv[0]);
+			exit(EXIT_SUCCESS);
 			break;
-		case '?':
-			print_usage(argv[0]);
+		case 'f':
+			strip.freq = atoi(optarg);
 			break;
-		default:
-			exit(EXIT_FAILURE);
+		case 'd':
+			strip.dmanum = atoi(optarg);
+			break;
+		case 'c':
+			strip.channel[0].count = atoi(optarg);
+			break;
+		case 'g':
+			strip.channel[0].gpionum = atoi(optarg);
+			break;
+		case 't':
+			if (!strcasecmp(optarg, "rgb"))
+				strip.channel[0].strip_type = WS2811_STRIP_RGB;
+			else if (!strcasecmp(optarg, "rbg"))
+				strip.channel[0].strip_type = WS2811_STRIP_RBG;
+			else if (!strcasecmp(optarg, "grb"))
+				strip.channel[0].strip_type = WS2811_STRIP_GRB;
+			else if (!strcasecmp(optarg, "gbr"))
+				strip.channel[0].strip_type = WS2811_STRIP_GBR;
+			else if (!strcasecmp(optarg, "bgr"))
+				strip.channel[0].strip_type = WS2811_STRIP_BGR;
+			else if (!strcasecmp(optarg, "brg"))
+				strip.channel[0].strip_type = WS2811_STRIP_BRG;
+			else if (!strcasecmp(optarg, "rgbw"))
+				strip.channel[0].strip_type = SK6812_STRIP_RGBW;
+			else if (!strcasecmp(optarg, "grbw"))
+				strip.channel[0].strip_type = SK6812_STRIP_GRBW;
+			else {
+				printf("invalid strip type: %s\n", optarg);
+				exit(EXIT_FAILURE);
+			}
+			break;
+		case 'i':
+			strip.channel[0].invert = 1;
+			break;
+		case 'b':
+			strip.channel[0].brightness = (uint8_t)atoi(optarg);
 			break;
 		}
 	}
@@ -156,6 +272,7 @@ int main(int argc, char* argv[]) {
 	// initialize Quick Pixel Control handlers
 	qpc_init();
 
+	// run the event loop
 	uv_run(loop, UV_RUN_DEFAULT);
 
 	return 0;
